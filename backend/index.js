19626e474ff6a2876d8409823afff0d3f99f7e37
YutaSato0717 <yutayuta0717@icloud.com>
2023-10-22 23:25:34 +0900

    expressの接続

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,15 @@
-//requireでexpressモジュールを読み込む
 const express = require('express');
-//expressモジュールを実体化して、定数appに代入
 const app = express();
-//ポート番号を指定
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-//'/'パスにGET要求があった際に実行する処理
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//3000ポートでlisten
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello World!' });
+  });
+
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
-})
+});
