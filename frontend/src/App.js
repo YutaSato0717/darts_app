4b52730f03c378220d1ad335cd8d543dae911060
YutaSato0717 <yutayuta0717@icloud.com>
2023-10-23 12:12:48 +0900

    バックとフロントの接続確認

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <div className="App">
       <h1>フロントエンド</h1>
+      <p>{message}</p>
     </div>
   );
 }
