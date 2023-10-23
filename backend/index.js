517161600279a82e5b6e431aeaac1979da82dc59
YutaSato0717 <yutayuta0717@icloud.com>
2023-10-23 15:11:39 +0900

    デプロイのための準備

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello World!' });
   });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });  
 
 app.listen(port, () => {
   console.log(`listening on *:${port}`);
