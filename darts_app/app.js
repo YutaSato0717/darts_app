f2be0c5be11c0c0b8c16ba84bb2987f96dd8b9be
takeru0076 <takeru200276@icloud.com>
2023-10-29 08:12:00 +0900

    signin機能の実装

--- darts_app/app.js
+++ darts_app/app.js
@@ -3,8 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieSession = require("cookie-session");
+const secret = "secretCuisine123";
 
 const app = express();
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [secret],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- darts_app/routes/home.js
+++ darts_app/routes/home.js
@@ -4,9 +4,27 @@ const knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'darts_app' });
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  console.log(`isAuth: ${isAuth}`);
+
+  knex("games")
+    .select("*")
+    .then(function (results) {
+      res.render('home', {
+        title: 'Darts App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('home', {
+        title: 'Darts App',
+      });
+    });
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
