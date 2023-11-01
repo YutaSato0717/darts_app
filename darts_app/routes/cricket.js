fb41f81f547e302ca47f597b8e20d15cb761c714
takeru0076 <takeru200276@icloud.com>
2023-11-01 12:22:47 +0900

    ルーティングの実装

---
+++ darts_app/routes/cricket.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  res.render('cricket', {
+    title: 'Darts App',
+    isAuth: isAuth,
+  });
+});
+
+module.exports = router;
