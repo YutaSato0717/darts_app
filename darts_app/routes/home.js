5c7cfbe3189c5fbe8afd947f1f82133c7146022c
takeru0076 <takeru200276@icloud.com>
2023-11-03 09:21:49 +0900

    /のログイン時のクエリ作成

--- darts_app/routes/home.js
+++ darts_app/routes/home.js
@@ -2,27 +2,57 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  knex("games")
-    .select("*")
-    .where({user_id: userId})
-    .then(function (results) {
-      console.log(results)
-      res.render('home', {
-        title: 'Darts App',
-        games: results,
-        isAuth: isAuth,
-      });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('home', {
-        title: 'Darts App',
-        isAuth: isAuth,
-      });
+
+  try {
+    // 最高の countup スコアを取得
+    const highestCountupScore = await knex('games')
+      .max('stats_or_score as highest_score')
+      .where({ user_id: userId, type: 'countup' })
+      .first();
+
+    // countup の平均スコアを計算
+    const averageCountupScore = await knex('games')
+      .where({ user_id: userId, type: 'countup' })
+      .avg('stats_or_score as avg_score')
+      .first();
+
+    // 01 の平均スコアを計算
+    const average01Stats = await knex('games')
+      .where({ user_id: userId, type: '01' })
+      .avg('stats_or_score as avg_score')
+      .first();
+
+    // cricket の平均スコアを計算
+    const averageCricketStats = await knex('games')
+      .where({ user_id: userId, type: 'cricket' })
+      .avg('stats_or_score as avg_score')
+      .first();
+
+    console.log('highestCountupScore:', highestCountupScore);
+    console.log('averageCountupScore:', averageCountupScore);
+    console.log('average01Stats:', average01Stats);
+    console.log('averageCricketStats', averageCricketStats);
+
+
+    // 結果をレンダリング
+    res.render('home', {
+      title: 'Darts App',
+      isAuth: isAuth,
+      highestCountupScore: highestCountupScore.highest_score,
+      averageCountupScore: averageCountupScore.avg_score || 0,
+      average01Stats: average01Stats.avg_score || 0,
+      averageCricketStats: averageCricketStats.avg_score || 0,
     });
+  } catch (err) {
+    console.error(err);
+    res.render('home', {
+      title: 'Darts App',
+      isAuth: isAuth,
+    });
+  }
 });
 
 router.post('/', function (req, res, next) {
