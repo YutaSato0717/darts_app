870d025589ca579778befa0e0165bec40bcd4097
takeru0076 <takeru200276@icloud.com>
2023-11-05 14:53:00 +0900

    cricketのクエリ作成

--- darts_app/routes/cricket.js
+++ darts_app/routes/cricket.js
@@ -1,13 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  res.render('cricket', {
-    title: 'Darts App',
-    isAuth: isAuth,
-  });
+
+  if (isAuth) {
+    // ログインしているユーザーのgamesテーブルから最大10件のデータを取得
+    const userGameData = await knex('games')
+      .select('stats_or_score')
+      .where({ type: 'cricket', user_id: userId })
+      .orderBy('id', 'desc')
+      .limit(10);
+
+    // 全ユーザーのgamesテーブルからstats_or_scoreの平均の上位5人のデータを取得
+    const top5Players = await knex('games')
+      .select('user_id', knex.raw('AVG(stats_or_score) as average'))
+      .where({ type: 'cricket' })
+      .groupBy('user_id')
+      .orderBy('average', 'desc')
+      .limit(5);
+
+    res.status(200).json({
+      userGameData,
+      top5Players,
+    });
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+});
+
+// POST関数: ログインしているユーザーのgamesテーブルにデータを追加
+router.post('/', async function (req, res, next) {
+  const userId = req.session.userid;
+
+  if (userId) {
+    // データを追加するクエリを実行
+    const { stats_or_score, win } = req.body; // リクエストからデータを取得
+    const gameData = {
+      type: 'cricket',
+      stats_or_score,
+      win,
+      user_id: userId,
+    };
+
+    // データを追加する前に、stats_or_scoreの値が正しいことを検証する
+    if (isNaN(stats_or_score)) {
+      res.status(400).json({ error: 'Stats or score must be a number.' });
+      return;
+    }
+
+    await knex('games')
+      .insert(gameData);
+    res.status(200).json({ message: 'Data added successfully.' });
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
 });
 
 module.exports = router;
